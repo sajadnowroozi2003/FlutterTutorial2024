3087af11a0aa7c6111daf5a5e9bdfd3391e846c8
sajad <sajad.nowroozi5442@gmail.com>
2024-07-27 01:43:15 +0430

    Vedio_DartProgrammin-3

---
+++ V-1/main.dart
@@ -0,0 +1,29 @@
+void main() {
+  var num = 22;
+  print(num);
+  print(num);
+  print(num);
+  print(num);
+  print(num);
+  print(num);
+  print(num);
+  print(num);
+  print(num);
+
+  String fullName = 'Sajad nowroozi 23444 @#\$%^^';
+  print(fullName);
+
+  int? x;
+  print(x);
+
+  late int y;
+  y = 30;
+  print(y);
+
+  final int xx;
+  xx = 50;
+
+  const int yy = 60;
+
+  bool test = false;
+}
